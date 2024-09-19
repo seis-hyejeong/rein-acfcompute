1d1ebb13df9918250446513ad3ec412d7911cf58
HyeJeong Kim <59041465+seis-hyejeong@users.noreply.github.com>
2024-09-19 17:39:43 -0600

    Add files via upload

---
+++ example/example.sh
@@ -0,0 +1,2 @@
+../src_comp.open/acf_water_comp_tauzin -d 0 -w 0.8 -m yellowstone.mod -p parameters.param -r rays.geom -o test
+
---
+++ example/rays.geom
@@ -0,0 +1,2 @@
+#baz [degree] ray parameter [s/m] NS displacement [m] EW displacement [m]
+0  6e-05 0 0
